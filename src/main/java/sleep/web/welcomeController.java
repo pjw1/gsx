a49e7ab593b1c6b67c98c1745d433ef0b7b27050
p1231 <p1231@DESKTOP-EA286VJ>
2017-07-31 16:28:00 +0900

    ini

---
+++ src/main/java/sleep/web/welcomeController.java
@@ -0,0 +1,14 @@
+package sleep.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class welcomeController {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello World!";
+	}
+
+}
